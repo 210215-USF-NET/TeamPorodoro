5c700ecee837cffa43c87787d5bbc60989153721
ZMFrench <inbox@zachfrench.com>
2021-03-12 17:04:41 -0500

    Timer Code attribution

--- PomMvc/Models/PomodoroCRVM.cs
+++ PomMvc/Models/PomodoroCRVM.cs
@@ -5,6 +5,12 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+/// <summary>
+/// A Pomodoro Timer built in Java script.  Code for the timer was modified from
+/// timer code on the following website:
+/// https://css-tricks.com/how-to-create-an-animated-countdown-timer-with-html-css-and-javascript/
+/// </summary>
 namespace PomMvc.Models
 {
     public class PomodoroCRVM

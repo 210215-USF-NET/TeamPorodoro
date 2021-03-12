e6ff4bd645e4b6f4672b29267a12e28b84ce5ef9
Connor <connoragnone@gmail.com>
2021-03-12 10:49:15 -0500

    Added user model

---
+++ PomModels/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SModels
+{
+    /// <summary>
+    /// Model for a User. Has properties for Id, Username, FirstName, and LastName
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserUserName { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+    }
+}

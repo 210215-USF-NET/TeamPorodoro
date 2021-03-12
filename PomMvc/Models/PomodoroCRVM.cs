3fcb813270d0ce05e3c8d6f0a7770cad34a1bb12
Ulices Esqueda <uesqueda756@gmail.com>
2021-03-12 12:25:06 -0800

    Porodoro controlelr

---
+++ PomMvc/Models/PomodoroCRVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PomMvc.Models
+{
+    public class PomodoroCRVM
+    {
+        [DisplayName("UserName")]
+        [Required]
+        public string UserName { get; set; }
+    }
+}
--- PomMvc/wwwroot/js/site.js
+++ PomMvc/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 0;
+    var id = setInterval(frame, 1000);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+            elem.innerHTML = width * 1 + '%';
+        }
+    }
+}

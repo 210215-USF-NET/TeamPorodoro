a44e396e56f3468ed7f1889294cbaf17e75db6e7
ZMFrench <inbox@zachfrench.com>
2021-03-12 10:48:33 -0500

    Added the NoteRecord model

---
+++ PomModels/NoteRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomModels
+{
+    class NoteRecord
+    {
+        public string UserName { get; set; }
+        public DateTime CreationTime { get; set; }
+        public string Note { get; set; }
+    }
+}

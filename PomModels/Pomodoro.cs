e33f73c7e4026288dde0feed6015ca697cb85d7d
Ulices Esqueda <uesqueda756@gmail.com>
2021-03-12 07:58:48 -0800

    added pomodoro

---
+++ PomModels/Pomodoro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomModels
+{
+    class Pomodoro
+    {
+        public string UserName { get; set; }
+        public DateTime CreationTime { get; set; }
+        public int PomodoroCounter { get; set; }
+    }
+}
